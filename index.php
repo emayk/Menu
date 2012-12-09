80bf40e13634d1d793bc8001e785f44d1aa1d02a
darko <darko@Jub>
2012-12-09 01:24:17 +0100

    updated example index.php

--- index.php
+++ index.php
@@ -13,4 +13,13 @@ $m->addMenuItem('Page 2-1', '/page/2/1', false, null, null, null, null, null, nu
 $m->addMenuItem('Page 2-2', '/page/2/2', false, null, null, null, null, null, null, null, false, true)->setIsLast();
 $m->addMenuItem('Page 3', '/page/3')->setIsLast();
 echo $m;
+/*
+ * Outputs
+ * <ul class="nav"><li class="active"><a href="/">Home</a></li>
+ * <li><a href="/page/1">Page 1</a></li>
+ * <li><a href="/page/2">Page 2</a>
+ * <ul class="nav"><li><a href="/page/2/1">Page 2-1</a></li>
+ * <li><a href="/page/2/2">Page 2-2</a></li></li></ul>
+ * <li><a href="/page/3">Page 3</a></li></li></ul>
+ */
 ?>
